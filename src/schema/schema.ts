145c857397d67ed4c87e1097522d50f927ff84c8
Kevin Bencs <kevinbencs8@gmail.com>
2026-04-20 21:28:23 +0200

    Created login and signup schemas.

---
+++ src/schema/schema.ts
@@ -0,0 +1,22 @@
+import * as z from "zod"
+
+
+export const signUpSchema = z.object({
+    name: z.string().min(1, {message: "Name is require"}),
+    email: z.email({message: "Email is required"}),
+    password: z.string().refine((val) =>{
+        const uppercase = /[A-Z]/.test(val);
+        const lowercase = /[a-z]/.test(val);
+        const number = /[0-9]/.test(val);
+
+        const length = val.length >= 8;
+
+        return length && uppercase && lowercase && number;
+
+    }, {message: "Password should be at least 8, contain 1 uppercase 1 lowercase and 1 number."})
+})
+
+export const loginSchema = z.object({
+    email: z.email({message: "Email is required"}),
+    password: z.string()
+})
