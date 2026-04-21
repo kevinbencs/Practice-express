35f0fa4424283cc75a1cb7af4f41343f16df561e
Kevin Bencs <kevinbencs8@gmail.com>
2026-04-21 19:41:39 +0200

    Changed tsconfig and installed ESLint and Prettier to ensure code adheres to consistent style guidelines and catches potential errors early.

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,13 @@ import { PORT } from "./config/config.ts";
 const app = express();
 
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log("Server is running on the " + PORT)
+})
+
+process.on("SIGTERM", () => {
+    console.log('SIGTERM signal received: closing HTTP server');
+    server.close(() => {
+        console.log("Server closed")
+    })
 })
